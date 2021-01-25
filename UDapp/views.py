77fcf1118e0dd8f9d71ee2b5e86214ad18c63fbe
VishalDeveloper17 <77782949+VishalDeveloper17@users.noreply.github.com>
2021-01-25 20:19:58 +0530

    initial signup page added

--- UDapp/views.py
+++ UDapp/views.py
@@ -6,4 +6,6 @@ def index(request):
 	return render(request,'pages/index.html')
 def login(request): 
 	return render(request,'pages/login.html')
+def signup(request): 
+	return render(request,'pages/signup.html')
 
