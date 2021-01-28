0442becbf752640c3ed087b3951e4acf7c98ce70
trying <trying@hard.com>
2021-01-28 15:18:40 +0530

    connections
    UDAdminApp connected with UDapp.
    Via login.html of 'UDapp' a person can go for dash.html of 'UDAdminApp'

--- UDapp/views.py
+++ UDapp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from UDAdminApp.views import *
 # Create your views here.
 
 def index(request):
 	return render(request,'pages/index.html')
-def login(request): 
+def login(request):
 	return render(request,'pages/login.html')
 def signup(request): 
 	return render(request,'pages/signup.html')
+
+def submit(request):
+	if request.method == 'POST':
+		name = request.POST['unm']
+		passw = request.POST['pwd']
+
+		context = {'name':name, 'passw':passw}
+		return render(request,'dash.html', context)
+		#return redirect('mainpage')
 
